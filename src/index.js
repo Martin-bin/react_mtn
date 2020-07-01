0460a83a3f5ad6ab9c4331843026ca8924e29b02
Rolando mota <mota@gmail.com>
2020-07-01 15:17:15 -0500

    Inicio del curso React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//const element = document.createElement('h1')
+//element.innerText = 'Hola mundo'
+//const container = document.getElementById('root')
+//container.appendChild(element)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const nombre ='Martin'
+
+const element = <h1>Dobles de riesgo {nombre}</h1>
+const container = document.getElementById('root')
+
+
+
+ReactDOM.render(element, container)
